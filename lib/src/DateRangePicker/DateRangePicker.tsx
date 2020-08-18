b0c70898b2d7d79af8e2d8d91da8bc97e550aad3
viactai <62006291+viactai@users.noreply.github.com>
2020-08-18 22:28:51 +0530

    Update DateRangePicker.tsx

--- lib/src/DateRangePicker/DateRangePicker.tsx
+++ lib/src/DateRangePicker/DateRangePicker.tsx
@@ -56,7 +56,7 @@ export const useDateRangeValidation = makeValidationHook<
   BaseDateRangePickerProps<any>
 >(validateDateRange, {
   defaultValidationError: [null, null],
-  isSameError: (a, b) => a[1] === b[1] && a[0] === b[0],
+  isSameError: false,
 });
 
 export function makeRangePicker<TWrapper extends SomeWrapper>(
